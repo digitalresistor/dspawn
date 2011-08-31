2ca281462fc16dd8c98d515268eae6f2bb83d1f8
Bert JW Regeer <bertjw@regeer.org>
2011-08-31 02:15:11 -0600

    Create files specifically for directory_entry
    There is no reason why that should be included with the main directory
    code and why it couldn't be compiled as a seperate module.
    
    Hook it up to the build within the Makefile as well...

---
+++ src/directory_entry.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2011 Bert JW Regeer;
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+**/
+
+#ifndef DIRECTORY_ENTRY_H_54BF50B3883F0A
+#define DIRECTORY_ENTRY_H_54BF50B3883F0A
+
+namespace dspawn
+{
+    /**
+     * @class directory_entry
+     * @brief A single entry in a directory
+     */
+    class directory_entry 
+    {
+        public:
+            directory_entry();
+            virtual ~directory_entry();
+    
+        private:
+            /* data */
+    };
+    
+    
+} /* namespace dspawn */
+
+
+#endif /* DIRECTORY_ENTRY_H_54BF50B3883F0A */
+
